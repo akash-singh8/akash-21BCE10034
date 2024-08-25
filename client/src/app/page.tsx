c2da9c07cad5ab1a9d20a8162f2f1a40d3b3e702
Akash Singh <developer.akash8@gmail.com>
2024-08-26 01:26:21 +0530

    use scss and added footer

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,15 @@
-import styles from "@/styles/page.module.css";
+import styles from "@/styles/page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>CHECKMATE</h1>
+
+      <footer className={styles.footer}>
+        <p>
+          a <a href="https://hitwicket.com/">HITWICKET</a> assignment
+        </p>
+      </footer>
     </main>
   );
 }
